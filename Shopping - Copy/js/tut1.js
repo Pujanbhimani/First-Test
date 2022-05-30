cb2bde9687e5d79a6a1a45b6e2a77d84c5749891
Pujanbhimani <105807671+Pujanbhimani@users.noreply.github.com>
2022-05-30 20:30:16 +0530

    Add files via upload

---
+++ Shopping - Copy/js/tut1.js
@@ -0,0 +1,24 @@
+
+
+// let booleanvar =true;
+// document.write(booleanvar , (typeof booleanvar));
+// console.log(booleanvar ,(typeof booleanvar));
+
+// let date = new Date();
+// document.write(date.toString());
+// console.log(date ,(typeof date));
+
+// // let numbervar = String([1,2,3,4,5]);
+// document.write(numbervar , (typeof numbervar))
+// console.log(numbervar.length , (typeof numbervar))
+
+
+let mystr = "20";
+let mynum = 32;
+
+console.log(mynum + mystr);
+
+
+// let D = "1023894";
+// // document.write(date.toSt ring());
+// console.log(D , (typeof D));
---
+++ Shopping - Copy/js/tut2.js
@@ -0,0 +1,1 @@
+document.write('This Is Tut 6')
